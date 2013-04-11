2ec35ab31e314ecf9063eb777ef766c7c4638452
Stanislav Demyanovich <stan@angrybubo.com>
2013-04-12 00:26:48 +0700

    Core header.
    Signed-off-by: Stanislav Demyanovich <stan@angrybubo.com>

---
+++ kruzhochki/Kruzhochki.h
@@ -0,0 +1,11 @@
+#ifndef KRUZHOCHKI_KRUZHOCHKI_H
+#define KRUZHOCHKI_KRUZHOCHKI_H
+
+#include "IRoot.h"
+#include "IGameStateManager.h"
+#include "IGameState.h"
+#include "IEventManager.h"
+#include "Event.h"
+#include "IGfxManager.h"
+
+#endif // KRUZHOCHKI_KRUZHOCHKI_H
