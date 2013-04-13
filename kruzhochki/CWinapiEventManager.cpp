8257779728294f9828c68f0ced07114e9d938d01
Stanislav Demyanovich <stan@angrybubo.com>
2013-04-13 14:18:37 +0700

    CWinapiEventManager::processWinapiEvent(...) is cleansed.
    Signed-off-by: Stanislav Demyanovich <stan@angrybubo.com>

--- kruzhochki/CWinapiEventManager.cpp
+++ kruzhochki/CWinapiEventManager.cpp
@@ -62,27 +62,31 @@ namespace kruz
       //Raising the kruz::Event corresponding to native WM_CLOSE event
       event.type = ET_SYSTEM_EVENT;
       SystemEvent se;
-      se.event = SYSTEM_EVENT::SE_WINDOW_CLOSE;
+      se.event = SE_WINDOW_CLOSE;
       event.systemEvent = se;
       raiseEvent(event);
       break;
     case WM_LBUTTONDOWN:
-      event.type = ET_MOUSE_INPUT;
-      MouseInput mi;
-      mi.input = MOUSE_INPUT::MI_LEFT_PRESSED;
-      mi.x = 0;
-      mi.y = 0;
-      event.mouseInput = mi;
-      raiseEvent(event);
+      {
+        event.type = ET_MOUSE_INPUT;
+        MouseInput mi;
+        mi.input = MI_LEFT_PRESSED;
+        mi.x = 0;
+        mi.y = 0;
+        event.mouseInput = mi;
+        raiseEvent(event);
+      }
       break;
     case WM_RBUTTONDOWN:
-      event.type = ET_MOUSE_INPUT;
-      MouseInput mi;
-      mi.input = MOUSE_INPUT::MI_RIGHT_PRESSED;
-      mi.x = 0;
-      mi.y = 0;
-      event.mouseInput = mi;
-      raiseEvent(event);
+      {
+        event.type = ET_MOUSE_INPUT;
+        MouseInput mi;
+        mi.input = MI_RIGHT_PRESSED;
+        mi.x = 0;
+        mi.y = 0;
+        event.mouseInput = mi;
+        raiseEvent(event);
+      }
       break;
     default:
       break;
